ee8eb63b9e30face6a0cc127305972a3a00f54a3
Shao Kun Deng <deng_shaokun@hotmail.com>
2016-03-10 13:31:31 -0500

    Add dummy code from prettyprinter to typechecker

--- src/goplusplus/Typechecker.java
+++ src/goplusplus/Typechecker.java
@@ -1,14 +1,21 @@
 package goplusplus;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+
 import goplusplus.analysis.DepthFirstAdapter;
 import goplusplus.node.*;
 
 public class Typechecker extends DepthFirstAdapter{
 	public Typechecker() {
-		
+		symbolTable = new LinkedList<HashMap<String, String> >();
 	}
 	
 	public static void check(Node node) {
 		node.apply(new Typechecker());
 	}
+	
+	LinkedList<HashMap<String, String> > symbolTable;
+	
+	
 }
