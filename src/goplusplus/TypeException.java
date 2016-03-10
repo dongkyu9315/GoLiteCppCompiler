0914ba6490fe1ad11a03da769b49e8329a1c8a4f
Dongkyu (Jason) Kim <dongkyukim@wpa171030.wireless.mcgill.ca>
2016-03-10 13:34:22 -0500

    TypeException java file added

---
+++ src/goplusplus/TypeException.java
@@ -0,0 +1,21 @@
+package goplusplus;
+
+public class TypeException extends RuntimeException {
+	public TypeException() { };
+	
+	public TypeException(String message) {
+		super(message);
+	}
+	
+	public TypeException(Throwable cause) {
+		super(cause);
+	}
+	
+	public TypeException(String message, Throwable cause) {
+		super(message, cause);
+	}
+	
+	public TypeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+}
